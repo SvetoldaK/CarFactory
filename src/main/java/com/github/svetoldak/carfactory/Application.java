961cbf9548f5515f26e2e1142fe0418dc362eea9
SvetoldaK <svetakurilova2001@mail.ru>
2022-01-07 17:55:58 +0300

    car

--- src/main/java/com/github/svetoldak/carfactory/Application.java
+++ src/main/java/com/github/svetoldak/carfactory/Application.java
@@ -10,6 +10,12 @@ import java.io.IOException;
 
 public class Application {
 
+    private static final int BRAND_INDEX = 0;
+    private static final int COLOR_INDEX = 1;
+    private static final int TYPE_INDEX = 2;
+    private static final int OPTION1_INDEX = 3;
+    private static final int OPTION2_INDEX = 4;
+
     public static void main(String[] args) throws FileNotFoundException {
 
         File csvData = new File("src/main/resources/data.csv");
@@ -19,12 +25,11 @@ public class Application {
             CSVParser csvParser = CSVParser.parse(fileReader, CSVFormat.newFormat(';'));
 
             for (CSVRecord csvRecord : csvParser) {
-                String brand = (csvRecord.get(0));
-                String color = (csvRecord.get(1));
-                int engineType = Integer.parseInt((csvRecord.get(2)));
-                String type = (csvRecord.get(3));
-                boolean option1 = Boolean.parseBoolean((csvRecord.get(4)));
-                boolean option2 = Boolean.parseBoolean((csvRecord.get(5)));
+                String brand = (csvRecord.get(BRAND_INDEX));
+                String color = (csvRecord.get(COLOR_INDEX));
+                String type = (csvRecord.get(TYPE_INDEX));
+                boolean option1 = Boolean.parseBoolean((csvRecord.get(OPTION1_INDEX)));
+                boolean option2 = Boolean.parseBoolean((csvRecord.get(OPTION2_INDEX)));
 
                 Calculator calculator = new Calculator();
 
